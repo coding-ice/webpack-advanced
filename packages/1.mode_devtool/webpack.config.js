18d0aa980b4d274ca6d27ccb6fc732655f1d2b8d
ice <1597834867@qq.com>
2024-01-06 19:40:42 +0800

    feat(mode_devtool): finish

--- packages/1.mode_devtool/webpack.config.js
+++ packages/1.mode_devtool/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 module.exports = {
   mode: 'production',
-  devtool: 'source-map',
+  devtool: "source-map",
   entry: path.resolve(__dirname, './src/main.js'),
   output: {
     path: path.resolve(__dirname, 'build'),
