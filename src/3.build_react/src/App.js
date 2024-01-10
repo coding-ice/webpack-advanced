5c60628a9c96b18f97ea15c115feaa2d9d5a5951
ice <1597834867@qq.com>
2024-01-10 14:46:03 +0800

    chore: change dir

---
+++ src/3.build_react/src/App.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { createRoot } from '../node_modules/react-dom/client.js'
+import Counter from './components/Counter.jsx'
+
+const root = createRoot(document.getElementById('root'))
+root.render(<Counter />)
---
+++ src/3.build_react/webpack.config.js
@@ -0,0 +1,25 @@
+const { resolve } = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  devtool: false,
+  entry: './src/App.js',
+  output: {
+    path: resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+  ],
+}
