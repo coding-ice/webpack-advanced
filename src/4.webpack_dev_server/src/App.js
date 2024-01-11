dc4613a957ac0b9213b56b0e27ba6ee96dcacb67
ice <1597834867@qq.com>
2024-01-11 20:23:06 +0800

    feat: dev-server finish

---
+++ src/4.webpack_dev_server/src/App.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { createRoot } from 'react-dom'
+// import { createRoot } from '../node_modules/react-dom/client.js'
+import Counter from './components/Counter.jsx'
+
+const root = createRoot(document.getElementById('root'))
+root.render(<Counter />)
---
+++ src/4.webpack_dev_server/src/components/Counter.jsx
@@ -0,0 +1,14 @@
+import React, { memo, useState } from 'react'
+
+const Counter = memo(() => {
+  const [count, setCount] = useState(0)
+  return (
+    <div className="count">
+      <button onClick={() => setCount(count + 1)}>+1</button>
+      <span style={{padding: '0 8px'}}>{count}</span>
+      <button onClick={() => setCount(count - 1)}>-1</button>
+    </div>
+  )
+})
+
+export default Counter
