a8ed385044d79b13f4fb7ed59bed385579e4b1cb
ice <1597834867@qq.com>
2024-01-23 23:03:29 +0800

    feat: cdn

---
+++ src/8.cdn/src/utils/sum.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const sum = (a, b) => a + b
+
+for (let i = 0; i < 10; i++) {
+  console.log(i)
+}
+
+export { sum }
---
+++ src/8.cdn/webpack.config.js
@@ -0,0 +1,36 @@
+const { resolve } = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'production',
+  devtool: false,
+  entry: './src/main.js',
+  output: {
+    // filename: '[name]_bundle.js',
+    path: resolve(__dirname, 'build'),
+    clean: true,
+  },
+  optimization: {
+    chunkIds: 'deterministic',
+    splitChunks: {
+      chunks: 'all',
+      minSize: 1 * 100,
+      cacheGroups: {
+        vendors: {
+          test: /node_modules/,
+          // filename: '[id]_vendors.bundle.js',
+        },
+        utils: {
+          test: /utils/,
+          filename: '[id]_utils.js',
+          minSize: 1
+        }
+      },
+    },
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: './index.html',
+    }),
+  ],
+}
