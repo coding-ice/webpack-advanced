6e5ba646ccb6773c9c729e1095f0587073824e9d
ice <1597834867@qq.com>
2024-02-01 23:24:14 +0800

    feat: css-minimizer finish

---
+++ src/11.css-minimizer/webpack.config.js
@@ -0,0 +1,25 @@
+const { resolve } = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  mode: 'development',
+  devtool: false,
+  entry: './src/main.js',
+  output: {
+    filename: 'js/[hash:6]_bundle.js',
+    path: resolve(__dirname, 'build'),
+    clean: true,
+  },
+  module: {
+    rules: [{ test: /\.css$/, use: [MiniCssExtractPlugin.loader, 'css-loader'] }],
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name]_[hash:6].css',
+    }),
+  ],
+}
