90adf1b520351fd5fc465d86615a03b2068bb096
ice <1597834867@qq.com>
2024-02-07 00:12:34 +0800

    feat: speed_measure finish

--- src/15.speed_measure/config/index.config.js
+++ src/15.speed_measure/config/index.config.js
@@ -3,12 +3,15 @@ const { merge } = require('webpack-merge')
 const htmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CompressionPlugin = require('compression-webpack-plugin')
+const SpeedMeasurePlugin = require('speed-measure-webpack-plugin')
 
 const prodWebpackConfig = require('./prod.config')
 const devWebpackConfig = require('./dev.config')
 
+const smp = new SpeedMeasurePlugin()
+
 const getCommonConfig = (isProd) => {
-  return {
+  return smp.wrap({
     entry: './src/main.js',
     output: {
       filename: 'js/[hash:6]_bundle.js',
@@ -16,7 +19,10 @@ const getCommonConfig = (isProd) => {
       clean: true,
     },
     module: {
-      rules: [{ test: /\.css$/, use: [isProd ? MiniCssExtractPlugin.loader : 'style-loader', 'css-loader'] }],
+      rules: [
+        { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ },
+        { test: /\.css$/, use: [isProd ? MiniCssExtractPlugin.loader : 'style-loader', 'css-loader'] },
+      ],
     },
     plugins: [
       new htmlWebpackPlugin({
@@ -25,10 +31,10 @@ const getCommonConfig = (isProd) => {
       new CompressionPlugin({
         test: /\.(css|js)$/,
         minRatio: 0.8,
-        algorithm: "gzip"
+        algorithm: 'gzip',
       }),
     ],
-  }
+  })
 }
 
 module.exports = function (env) {
