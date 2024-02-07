5a32523acaf26e7b4b62970b6b0e070913262cb4
ice <1597834867@qq.com>
2024-02-07 23:24:12 +0800

    feat: 16.bundle_analyzer

---
+++ src/16.bundle_analyzer/config/index.config.js
@@ -0,0 +1,47 @@
+const { resolve } = require('path')
+const { merge } = require('webpack-merge')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CompressionPlugin = require('compression-webpack-plugin')
+const SpeedMeasurePlugin = require('speed-measure-webpack-plugin')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+
+const prodWebpackConfig = require('./prod.config')
+const devWebpackConfig = require('./dev.config')
+
+const smp = new SpeedMeasurePlugin()
+
+const getCommonConfig = (isProd) => {
+  return smp.wrap({
+    entry: './src/main.js',
+    output: {
+      filename: 'js/[hash:6]_bundle.js',
+      path: resolve(__dirname, '../build'),
+      clean: true,
+    },
+    module: {
+      rules: [
+        { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ },
+        { test: /\.css$/, use: [isProd ? MiniCssExtractPlugin.loader : 'style-loader', 'css-loader'] },
+      ],
+    },
+    plugins: [
+      new htmlWebpackPlugin({
+        template: './index.html',
+      }),
+      new CompressionPlugin({
+        test: /\.(css|js)$/,
+        minRatio: 0.8,
+        algorithm: 'gzip',
+      }),
+      new BundleAnalyzerPlugin(),
+    ],
+  })
+}
+
+module.exports = function (env) {
+  const isProd = env.production
+  const config = isProd ? prodWebpackConfig : devWebpackConfig
+
+  return merge(getCommonConfig(isProd), config)
+}
